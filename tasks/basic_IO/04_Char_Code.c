0f54a07b331a2ebc35728390a434bd1306b6e805
Artemij <100249321+U-ND3R@users.noreply.github.com>
2025-08-23 19:09:23 +0300

    Create 04_Char_Code.c
    4th Task with scanning a character and printing its ASCII code.

---
+++ tasks/basic_IO/04_Char_Code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  // Task: scan a character and print its ASCII code
+  /* 
+    EXAMPLE:
+    Input: A
+    Output: A -> 65
+  */
+  
+  scanf("%c", &symbol);
+  printf("%c -> %d", symbol, symbol);
+  return 0;
+}
