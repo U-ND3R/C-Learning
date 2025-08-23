84777855ee8f023252f666485945f4109929fdad
Artemij <100249321+U-ND3R@users.noreply.github.com>
2025-08-23 18:26:42 +0300

    Create 03_Echo_Number.c
    3rd Task with scanning an integer and printing it back

---
+++ tasks/basic_IO/03_Echo_Number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  // Task: scan an integer and print it back
+  /* 
+    EXAMPLE:
+    Input: 42
+    Output: You entered: 42
+  */
+  int x;
+  scanf("%d", &x);
+  printf("\nYou entered: %d", x);
+  return 0;
+}
