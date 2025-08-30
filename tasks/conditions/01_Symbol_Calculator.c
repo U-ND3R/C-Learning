7330984001d54e823508b7a5bf7bb6bee9cbbccc
Artemij <100249321+U-ND3R@users.noreply.github.com>
2025-08-30 18:20:36 +0300

    Create 01_Symbol_Calculator.c
    1st Task with scanning 2 integers and an operator `(+, -, /, %, *)`, performing the corresponding operation and outputing the result (`"Unknown operator"` if the symbol is unknown)

---
+++ tasks/conditions/01_Symbol_Calculator.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){
+    /* 
+    Task: Scan 2 integers and an operator (+, -, *, /), perform the corresponding operation and output the result
+    If the symbol is unknown, output "Unknown operator"
+    */
+    /*
+      EXAMPLE: 
+      Please print two numbers and an operator (+, -, /, %, *): 5 3 +
+      Addition of x and y is 8.0
+
+      Please print two numbers and an operator (+, -, /, %, *): 4 7 ?
+      Unknown operator
+    */
+    int x, y;
+    char symbol;
+    float result;
+    printf("Please print two numbers and an operator (+, -, /, %, *): ");
+    scanf ("%d %d %c", &x, &y, &symbol);
+    if (symbol == '+'){
+        result = x + y;
+        printf("Addition of x and y is %.1f", result);
+    } else if (symbol == '-'){
+        result = x - y;
+        printf("Substraction of x and y is %.1f", result);
+    } else if (symbol == '/'){
+        result = x / y;
+        printf("Division of x and y is %.1f", result);
+    } else if (symbol == '%'){
+        result = x / y;
+        printf("Modulus of x and y is %.1f", result);
+    } else if (symbol == '*'){
+        result = x / y;
+        printf("Multiplication of x and y is %.1f", result);
+    } else {
+        printf("Unknown operator");
+    }
+}
